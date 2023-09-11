4396949c5c4a987628bceb5792391d3585c97cda
sokchamreun.phim <phim.sokchamreun@gmail.com>
2023-09-11 21:59:24 +0700

    add ISO C Random

---
+++ ISOCRandom.cs
@@ -0,0 +1,26 @@
+﻿namespace SlashX.Net
+{
+    public class ISOCRandom
+    {
+        public static int Generate(uint seed)
+        {
+            uint next = seed;
+            int result;
+
+            next *= 1103515245;
+            next += 12345;
+            result = (int)((uint)(next / 65536) % 2048);
+
+            next *= 1103515245;
+            next += 12345;
+            result <<= 10;
+            result ^= (int)((uint)(next / 65536) % 1024);
+
+            next *= 1103515245;
+            next += 12345;
+            result <<= 10;
+            result ^= (int)((uint)(next / 65536) % 1024);
+            return result;
+        }
+    }
+}
