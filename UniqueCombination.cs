a63e42206a3a7e0a86b8869e438e2d2522e7f784
sokchamreun.phim <phim.sokchamreun@gmail.com>
2023-09-12 22:28:27 +0700

    UniqueCombination

---
+++ UniqueCombination.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlashX.Net
+{
+    public class UniqueCombination
+    {
+        public static List<List<string>> Generate(string[] arr)
+        {
+            List<List<string>> combinations = new List<List<string>>();
+            int length = arr.Length;
+
+            for (int i = 0; i < (1 << length); ++i)
+            {
+                List<string> combination = new List<string>();
+                int count;
+                for (count = 0; count < length; ++count)
+                {
+                    if ((i & 1 << count) > 0)
+                        combination.Add(arr[count]);
+                }
+
+                if (count > 0 && combination.Count > 0)
+                    combinations.Add(combination);
+            }
+
+            return combinations;
+        }
+    }
+}
