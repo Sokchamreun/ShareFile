086f9a3007976d425d844a695979c2bb3ebe4e22
sokchamreun <phim.sokchamreun@gmail.com>
2023-09-23 16:24:25 +0700

    add LinearSearch

---
+++ LinearSearch.cs
@@ -0,0 +1,19 @@
+﻿namespace SlashX.Net
+{
+    /// <summary>
+    /// Linear search, also known as sequential search is an algorithm for finding a target value within a list. 
+    /// It sequentially checks each element of the list for the target value until a match is found or until all the elements have been searched. 
+    /// </summary>
+    public class LinearSearch
+    {
+        public static int Search(int[] list, int data)
+        {
+            for (int i = 0; i < list.Length; i++)
+            {
+                if (data == list[i]) return i;
+            }
+
+            return -1;
+        }
+    }
+}
