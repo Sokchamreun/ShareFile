4894dd0efa59c787fff4de18c48dd3f815161485
sokchamreun <phim.sokchamreun@gmail.com>
2023-09-28 21:27:03 +0700

    add bubble sort

---
+++ BubbleSort.cs
@@ -0,0 +1,26 @@
+﻿namespace SlashX.Net
+{
+    /// <summary>
+    /// Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements if they are in the wrong order. 
+    /// This algorithm is not suitable for large data sets as its average and worst-case time complexity is quite high.
+    /// </summary>
+    public class BubbleSort
+    {
+        public static int[] Sort(int[] data)
+        {
+            for (int i = 1; i < data.Length; ++i)
+            {
+                for (int j = 0; j < data.Length - i; ++j)
+                {
+                    if (data[j] > data[j + 1])
+                    {
+                        data[j] ^= data[j + 1];
+                        data[j + 1] ^= data[j];
+                        data[j] ^= data[j + 1];
+                    }
+                }
+            }
+            return data;
+        }
+    }
+}
