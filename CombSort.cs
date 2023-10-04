1dea6949aa34a65b6452c21897ff4a409c4e4e77
sokchamreun <phim.sokchamreun@gmail.com>
2023-10-04 21:23:02 +0700

    add caomb and countin sort

---
+++ CombSort.cs
@@ -0,0 +1,42 @@
+﻿namespace SlashX.Net
+{
+    /// <summary>
+    /// Comb sort is sorting algorithm and it is a variant of Bubble sort,
+    /// the Comb Sort increases the gap used in comparisons and exchanges.
+    /// </summary>
+    public class CombSort
+    {
+        public static int[] Sort(int[] data)
+        {
+            double gap = data.Length;
+            bool swaps = true;
+
+            while (gap > 1 || swaps)
+            {
+                gap /= 1.247330950103979;
+
+                if (gap < 1)
+                    gap = 1;
+
+                int i = 0;
+                swaps = false;
+
+                while (i + gap < data.Length)
+                {
+                    int igap = i + (int)gap;
+
+                    if (data[i] > data[igap])
+                    {
+                        int temp = data[i];
+                        data[i] = data[igap];
+                        data[igap] = temp;
+                        swaps = true;
+                    }
+
+                    ++i;
+                }
+            }
+            return data;
+        }
+    }
+}
---
+++ CountingSort.cs
@@ -0,0 +1,31 @@
+﻿namespace SlashX.Net
+{
+    /// <summary>
+    /// Counting sort is sorting algorithm based on keys between specific range. It works by counting the number of objects having distinct key values. 
+    /// Then doing some arithmetic to calculate the position of each object in the output sequence.
+    /// </summary>
+    public class CountingSort
+    {
+        public static int[] Sort(int[] data, int min, int max)
+        {
+            int[] count = new int[max - min + 1];
+            int z = 0;
+
+            for (int i = 0; i < count.Length; i++)
+                count[i] = 0;
+
+            for (int i = 0; i < data.Length; i++)
+                count[data[i] - min]++;
+
+            for (int i = min; i <= max; i++)
+            {
+                while (count[i - min]-- > 0)
+                {
+                    data[z] = i;
+                    ++z;
+                }
+            }
+            return data;
+        }
+    }
+}
